99dd904f2b0b8f586bf707f8e7375a5f40d9fedb
Divyansh Narang <147334677+divyansh0810@users.noreply.github.com>
2026-03-06 15:28:28 +0530

    Update conftest.py

--- conftest.py
+++ conftest.py
@@ -7,22 +7,41 @@ results = []
 
 @pytest.hookimpl(hookwrapper=True)
 def pytest_runtest_makereport(item, call):
-    
+
     start_time = datetime.now()
 
     outcome = yield
     report = outcome.get_result()
-    
+
     end_time = datetime.now()
 
     if report.when == "call":
+
+        error_message = ""
+        screenshot_path = ""
+
+        if report.failed:
+
+            error_message = report.longrepr.reprcrash.message
+
+            page = item.funcargs.get("page", None)
+
+            if page:
+                os.makedirs("reports/screenshots", exist_ok=True)
+
+                screenshot_name = f"{item.name}_{datetime.now().strftime('%Y%m%d_%H%M%S')}.png"
+                screenshot_path = f"reports/screenshots/{screenshot_name}"
+
+                page.screenshot(path=screenshot_path)
 
         results.append({
             "Test Name": item.name,
             "Status": report.outcome,
             "Start Time": start_time.strftime("%H:%M:%S"),
             "End Time": end_time.strftime("%H:%M:%S"),
-            "Duration (sec)": round(report.duration, 2)
+            "Duration (sec)": round(report.duration, 2),
+            "Error": error_message,
+            "Screenshot": screenshot_path
         })
 
 
