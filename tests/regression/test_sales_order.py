a4b5d46beb610e6854fb33680ff9d2c6e1331113
Divyansh Narang <147334677+divyansh0810@users.noreply.github.com>
2026-03-06 15:26:58 +0530

    Update test_sales_order.py

--- tests/regression/test_sales_order.py
+++ tests/regression/test_sales_order.py
@@ -1,38 +1,37 @@
-import pytest
 import os
 from playwright.sync_api import sync_playwright
 from dotenv import load_dotenv
+import pytest
 
 load_dotenv()
 
 @pytest.mark.regression
+
 def test_open_sales_order():
-
     with sync_playwright() as p:
-
-        browser = p.chromium.launch(headless=True, slow_mo=800)
+        browser = p.chromium.launch(headless=False, slow_mo=800)
         page = browser.new_page()
+        page.set_default_timeout(60000)
 
         page.goto("https://test-vidirav15.frappe.cloud/login")
 
-        # Fetch credentials
-        user = os.getenv("ERP_USER")
-        password = os.getenv("ERP_PASS")
 
-        # Validate environment variables
-        assert user is not None, "ERP_USER not set"
-        assert password is not None, "ERP_PASS not set"
-
-        # Fill login form
-        page.fill("#login_email", user)
-        page.fill("#login_password", password)
-
+        page.fill("#login_email", os.getenv("ERP_USER"))
+        page.fill("#login_password", os.getenv("ERP_PASS"))
         page.click("button:has-text('Login')")
 
-        page.wait_for_load_state("networkidle")
 
-        page.goto("https://test-vidirav15.frappe.cloud/app/sales-order")
+        page.wait_for_load_state("networkidle")
+        page.locator("a").filter(has_text="ERPNext").click()
+        page.get_by_role("link", name="Selling").click()
+        page.get_by_role("link", name="Sales Order", exact=True).click()
+
+        # page.goto("https://test-vidirav15.frappe.cloud/app/sales-order")
+
 
         page.wait_for_selector("text=Sales Order")
 
+        print("Sales Order page opened successfully")
+
         browser.close()
+
